d965908987000a74ee5a85fdd6ffd6a3aa469eae
Kaitlin Regan <karegan@gmail.com>
2020-11-11 13:04:14 -0500

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
+configure do
+    enable :sessions
+    set :session_secret, "secret"
+end
 
 class App < Sinatra::Base
+    get '/' do
+        erb :index
+    end
+
+    # post '/checkout' do
+    #     params.to_s
+    # end
+    post '/checkout' do
+        @session = session
+        session[:item] = params[:item]
+        params[:item] + @session
+    end
 end
